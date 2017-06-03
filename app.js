095ffbfe00f45ff47febed68ad854a4ddfc40af5
Jonathan Herman <jdh11235@gmail.com>
2017-06-03 01:15:33 -0700

    add codes to names table

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
 function App() {
+  const CODES_TO_NAMES = {
+    "0": "black",
+    "1": "dark_blue",
+    "2": "dark_green",
+    "3": "dark_aqua",
+    "4": "dark_red",
+    "5": "dark_purple",
+    "6": "gold",
+    "7": "gray",
+    "8": "dark_gray",
+    "9": "blue",
+    "a": "green",
+    "b": "aqua",
+    "c": "red",
+    "d": "light_purple",
+    "e": "yellow",
+    "f": "white",
+    "k": "obfuscated",
+    "l": "bold",
+    "m": "strikethrough",
+    "n": "underline",
+    "o": "italic"
+  }
+
   function fcodesToJson(text_fcodes) {
     text_fcodes = '&f' + text_fcodes
     text_fcodes = text_fcodes.replace(/[&§]r/gi, '&f')
